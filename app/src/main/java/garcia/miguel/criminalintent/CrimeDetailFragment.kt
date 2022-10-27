ab4607725ffce410cb74eec1bfecd879ee99da66
Miguel García <ingjmiguelgr@gmail.com>
2022-10-27 11:38:27 -0500

    Crime27/10

--- app/src/main/java/garcia/miguel/criminalintent/CrimeDetailFragment.kt
+++ app/src/main/java/garcia/miguel/criminalintent/CrimeDetailFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
 import garcia.miguel.criminalintent.databinding.FragmentCrimeDetailBinding
 import java.util.UUID
@@ -14,7 +15,12 @@ class CrimeDetailFragment:Fragment() {
 
     private lateinit var crime: Crime
 
-    private lateinit var binding: FragmentCrimeDetailBinding
+    private var _binding: FragmentCrimeDetailBinding? = null
+    private val binding
+        get() = checkNotNull(_binding) {
+            "Cannot access binding because it is null. Is the view visible?"
+        }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,6 +30,7 @@ class CrimeDetailFragment:Fragment() {
             title = "",
             date = Date(),
             isSolved = false
+        )
     }
 
     override fun onCreateView(
@@ -31,8 +38,30 @@ class CrimeDetailFragment:Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding =
+        _binding =
             FragmentCrimeDetailBinding.inflate(layoutInflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.apply {
+            crimeTitle.doOnTextChanged { text, _, _, _ ->
+                crime = crime.copy(title = text.toString())
+            }
+
+            crimeDate.apply {
+                text = crime.date.toString()
+                isEnabled = false
+            }
+
+            crimeSolved.setOnCheckedChangeListener { _, isChecked ->
+                crime = crime.copy(isSolved = isChecked)
+            }
+        }
+    }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
